ef04f08493899dbff7aea5e3b10643515c07731e
Krishna Sakinala <ep.krishna@gmail.com>
2017-09-20 07:48:13 +0530

    Added test case executable or not logic and taking screenshot for the failed tests

--- src/test/java/com/automationtesting/basetest/BaseTest.java
+++ src/test/java/com/automationtesting/basetest/BaseTest.java
@@ -1,18 +1,26 @@
 package com.automationtesting.basetest;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import com.automationtesting.util.ExcelApiTest;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.markuputils.ExtentColor;
@@ -30,9 +38,13 @@ public class BaseTest {
 
 	public FileInputStream fis = null;
 	public Properties prop = null;
+	public ExcelApiTest eat = null;
 
 	@BeforeSuite
-	public void init() throws IOException {
+	public void init() throws Exception {
+
+		eat = new ExcelApiTest(
+				System.getProperty("user.dir") + "/src/main/java/com/automationtesting/repo/TestData.xlsx");
 
 		fis = new FileInputStream(
 				"/Users/krishnasakinala/hubiC/workspace/POMWPF_Framework/src/main/java/com/automationtesting/repo/config.properties");
@@ -76,11 +88,13 @@ public class BaseTest {
 	}
 
 	@AfterMethod
-	public void getResult(ITestResult result) {
+	public void getResult(ITestResult result) throws IOException {
 		if (result.getStatus() == ITestResult.FAILURE) {
+			String screenShotPath = capture(driver, "screenShotName");
 			test.fail(MarkupHelper.createLabel(result.getName() + " Test case FAILED due to below issues:",
 					ExtentColor.RED));
 			test.fail(result.getThrowable());
+			test.fail("Snapshot below: " + test.addScreenCaptureFromPath(screenShotPath));
 		} else if (result.getStatus() == ITestResult.SUCCESS) {
 			test.pass(MarkupHelper.createLabel(result.getName() + " Test Case PASSED", ExtentColor.GREEN));
 		} else {
@@ -89,8 +103,31 @@ public class BaseTest {
 		}
 	}
 
+	public void verifyTitle(String title) {
+		Assert.assertEquals(driver.getTitle(), title);
+	}
+
+	public static String capture(WebDriver driver, String screenShotName) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String dest = System.getProperty("user.dir") + "/ErrorScreenshots/" + screenShotName + getCurrentDateTime()
+				+ ".png";
+		File destination = new File(dest);
+		FileUtils.copyFile(source, destination);
+
+		return dest;
+	}
+
+	public static String getCurrentDateTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		Date date = new Date();
+
+		return sdf.format(date);
+	}
+
 	@AfterSuite
 	public void tearDown() {
 		extent.flush();
+		driver.quit();
 	}
 }
